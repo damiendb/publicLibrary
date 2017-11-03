5f508dbb93f7125e2b4e0180a0fcba96f1572451
Damien <damiendb@hotmail.com>
2017-11-03 17:41:15 +0800

    Expand the Book Model
    Added Author and Full Title

--- src/model/book.js
+++ src/model/book.js
@@ -1,5 +1,9 @@
 function Book( slots) {
   this.isbn = slots.isbn;
   this.title = slots.title;
+  this.author - slots.author;
   this.year = slots.year;
+  this.getFullTitle: function () {
+  	return this.title + " written by " + this.author;
+  }
 };
